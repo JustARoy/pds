0723b6d3f03aea67f461bb3bdf6f2cceaeab7205
JustARoy <tim.eickenroth@gmail.com>
2024-09-12 00:21:18 +0200

    Fixing Calculation Error and removing old Attributes and groups

--- module/actor-sheet.js
+++ module/actor-sheet.js
@@ -42,22 +42,7 @@ export class SimpleActorSheet extends ActorSheet {
     
     super.activateListeners(html);
 
-    if(this.actor.owner){
-      html.find(".item-roll").click(this._onItemRoll.bind(this));
-    }
-
-    // Everything below here is only needed if the sheet is editable
-    if ( !this.isEditable ) return;
-
-    // Attribute Management
-    //html.find(".skills").on("click", "a.skills-roll", EntitySheetHelper.onSkillRoll.bind(this));
-
-    // Item Controls
-    html.find(".item-control").click(this._onItemControl.bind(this));
-    html.find(".items .rollable").on("click", this._onItemRoll.bind(this));
-
     //Rollable abilities
-    html.on('click', '.rollable', this._onRoll.bind(this));
     html.on('click', '.dmg-roll', this._onRollDmg.bind(this));
     html.on('click', '.armor-roll', this._onRollArmor.bind(this));
 
@@ -68,14 +53,6 @@ export class SimpleActorSheet extends ActorSheet {
       html.find(".skill-roll").click(EntitySheetHelper.onSkillRollNew.bind(this));
     }
 
-    // Add draggable for Macro creation
-    html.find(".attributes a.attribute-roll").each((i, a) => {
-      a.setAttribute("draggable", true);
-      a.addEventListener("dragstart", ev => {
-        let dragData = ev.currentTarget.dataset;
-        ev.dataTransfer.setData('text/plain', JSON.stringify(dragData));
-      }, false);
-    });
   }
 
   async _onRollArmor(event){
@@ -131,82 +108,9 @@ export class SimpleActorSheet extends ActorSheet {
 
   /* -------------------------------------------- */
 
-  /**
-   * Handle click events for Item control buttons within the Actor Sheet
-   * @param event
-   * @private
-   */
-  _onItemControl(event) {
-    event.preventDefault();
-
-    // Obtain event data
-    const button = event.currentTarget;
-    const li = button.closest(".item");
-    const item = this.actor.items.get(li?.dataset.itemId);
-
-    // Handle different actions
-    switch ( button.dataset.action ) {
-      case "create":
-        const cls = getDocumentClass("Item");
-        return cls.create({name: game.i18n.localize("SIMPLE.ItemNew"), type: "item"}, {parent: this.actor});
-      case "edit":
-        return item.sheet.render(true);
-      case "delete":
-        return item.delete();
-    }
-  }
-
-  /* -------------------------------------------- */
-
-  /**
-   * Listen for roll buttons on items.
-   * @param {MouseEvent} event    The originating left click event
-   */
- _onItemRoll(event) {
-   let button = $(event.currentTarget);
-   const li = button.parents(".item");
-   const item = this.actor.items.get(li.data("itemId"));
-   let r = new Roll(button.data('roll'), this.actor.getRollData());
-   return r.toMessage({
-     user: game.user.id,
-     speaker: ChatMessage.getSpeaker({ actor: this.actor }),
-     flavor: `<h2>${item.name}</h2><h3>${button.text()}</h3>`
-   });
- }
-
-_onRoll(event) {
-  event.preventDefault();
-  const element = event.currentTarget;
-  const dataset = element.dataset;
-
-  // Handle item rolls.
-  if (dataset.rollType) {
-    if (dataset.rollType == 'item') {
-      const itemId = element.closest('.item').dataset.itemId;
-      const item = this.actor.items.get(itemId);
-      if (item) return item.roll();
-    }
-  }
-
-  // Handle rolls that supply the formula directly.
-  if (dataset.roll) {
-    let label = dataset.label ? `[ability] ${dataset.label}` : '';
-    let roll = new Roll(dataset.roll, this.actor.getRollData());
-    roll.toMessage({
-      speaker: ChatMessage.getSpeaker({ actor: this.actor }),
-      flavor: label,
-      rollMode: game.settings.get('core', 'rollMode'),
-    });
-    return roll;
-  }
-}
-
-  /* -------------------------------------------- */
-
   /** @inheritdoc */
   _getSubmitData(updateData) {
     let formData = super._getSubmitData(updateData);
-    formData = EntitySheetHelper.updateAttributes(formData, this.object);
     return formData;
   }
 }
